c0faf31734089c127b2c58d062d659d045701a38
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-05-13 18:09:38 -0400

    4x is an easier factor than 8x to discern at a glance

--- Demo_EnumerableDeferredTaskExec/Program.cs
+++ Demo_EnumerableDeferredTaskExec/Program.cs
@@ -8,34 +8,35 @@ using BenchmarkDotNet.Running;
 // AMD Ryzen Threadripper 2920X, 1 CPU, 24 logical and 12 physical cores
 //     .NET SDK=6.0.100
 //     [Host]     : .NET 6.0.0 (6.0.21.52210), X64 RyuJIT
-// Job-UOZRCX : .NET 6.0.0 (6.0.21.52210), X64 RyuJIT
+// Job-TZRWKD : .NET 6.0.0 (6.0.21.52210), X64 RyuJIT
 //
 //     InvocationCount=1  UnrollFactor=1  
 //
-//     |         Method | TaskCount |      Mean |    Error |   StdDev |    Median | Allocated |
-//     |--------------- |---------- |----------:|---------:|---------:|----------:|----------:|
-//     | FullConcurrent |       512 |  13.76 ms | 0.269 ms | 0.442 ms |  13.77 ms |    203 KB |
-//     |        Batched |       512 | 123.80 ms | 2.501 ms | 7.376 ms | 118.69 ms |    201 KB |
+//     |         Method | TaskCount |     Mean |    Error |   StdDev | Allocated |
+//     |--------------- |---------- |---------:|---------:|---------:|----------:|
+//     | FullConcurrent |       512 | 13.88 ms | 0.272 ms | 0.334 ms |    204 KB |
+//     |        Batched |       512 | 58.76 ms | 0.587 ms | 0.490 ms |    200 KB |
 //
 // // * Warnings *
-//     MultimodalDistribution
-// Bench.Batched: InvocationCount=1, UnrollFactor=1 -> It seems that the distribution is bimodal (mValue = 3.23)
 //     MinIterationTime
-// Bench.FullConcurrent: InvocationCount=1, UnrollFactor=1 -> The minimum observed iteration time is 11.6975 ms which is very small. It's recommended to increase it to at least 100.0000 ms using more operati
+// Bench.FullConcurrent: InvocationCount=1, UnrollFactor=1 -> The minimum observed iteration time is 13.3797 ms which is very small. It's recommended to increase it to at least 100.0000 ms using more operati
+// ons.
+//     Bench.Batched: InvocationCount=1, UnrollFactor=1        -> The minimum observed iteration time is 57.7780 ms which is very small. It's recommended to increase it to at least 100.0000 ms using more operati
 // ons.
 //
 // // * Hints *
 //     Outliers
-// Bench.FullConcurrent: InvocationCount=1, UnrollFactor=1 -> 5 outliers were removed, 6 outliers were detected (11.70 ms, 15.08 ms..29.09 ms)
+// Bench.FullConcurrent: InvocationCount=1, UnrollFactor=1 -> 3 outliers were removed (25.54 ms..28.92 ms)
+// Bench.Batched: InvocationCount=1, UnrollFactor=1        -> 4 outliers were removed (73.47 ms..74.37 ms)
 //
 // // * Legends *
 // TaskCount : Value of the 'TaskCount' parameter
 // Mean      : Arithmetic mean of all measurements
 // Error     : Half of 99.9% confidence interval
 // StdDev    : Standard deviation of all measurements
-// Median    : Value separating the higher half of all measurements (50th percentile)
 // Allocated : Allocated memory per single operation (managed only, inclusive, 1KB = 1024B)
 // 1 ms      : 1 Millisecond (0.001 sec)
+
 
 class Program
 {
@@ -63,11 +64,11 @@ public class Bench
     [Benchmark]
     public int[] FullConcurrent() => Task.WhenAll(Tasks.Select(f => f)).Result;
 
-    // If deferred execution is happening, this should take approx 8x as long as `FullConcurrent`
+    // If deferred execution is happening, this should take approx 4x as long as `FullConcurrent`
     [Benchmark]
     public int[][] Batched()
     {
-        var batchSize = TaskCount / 8;
+        var batchSize = TaskCount / 4;
         var batchCount = (TaskCount + (batchSize - 1)) / batchSize; // rounded up
 
         int[][] results = new int[batchCount][];
