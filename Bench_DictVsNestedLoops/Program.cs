d4f8fdf3f497369a06c89b3c0aaa3f7618c85833
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2022-07-17 14:30:58 -0400

    Add `Bench_DictVsNestedLoops`

---
+++ Bench_DictVsNestedLoops/Program.cs
@@ -0,0 +1,73 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Bench_DictVsNestedLoops;
+
+public class Container
+{
+    public string A { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Bench>();
+    }
+}
+
+[MemoryDiagnoser]
+public class Bench
+{
+    private const int RandSeed = 826528;
+    private const int BenchmarkAccessCount = 1000;
+
+    [Params(10, 100, 1000, 10000, 100000)]
+    public int MaxItems { get; set; }
+
+    private string[] _randomKeys = null!;
+
+    private List<KeyValuePair<string, Container>> _keyValuePairs;
+        
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        _keyValuePairs = Enumerable.Range(0, MaxItems)
+            .Select(i => new KeyValuePair<string, Container>(i.ToString(), new Container(){ A = i.ToString()}))
+            .ToList();
+            
+        // Init keys to access in benchmarks
+        var r = new Random(RandSeed);
+        _randomKeys = Enumerable.Range(0, BenchmarkAccessCount)
+            .Select(_ => r.Next(MaxItems).ToString())
+            .ToArray();
+    }
+
+    [Benchmark]
+    public Container? Dictionary()
+    {
+        Container? almostANoOp = null;
+        var dictionary = new Dictionary<string, Container>(_keyValuePairs);
+        foreach (var key in _randomKeys) {
+            almostANoOp = dictionary[key];
+        }
+
+        return almostANoOp;
+    }
+
+    [Benchmark]
+    public Container? Loops()
+    {
+        Container? almostANoOp = null;
+        foreach (var kv in _keyValuePairs) {
+            foreach (var key in _randomKeys) {
+                if (kv.Key == key) {
+                    almostANoOp = kv.Value;
+                }
+            }
+        }
+
+        return almostANoOp;
+    }
+
+}
