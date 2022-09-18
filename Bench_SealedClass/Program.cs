dcf6176291497b28a666cffb2bff49c83db3fe6c
John Paul Rutigliano <jp@jpcode.dev>
2022-09-17 22:31:33 -0400

    Bench_SealedClass

---
+++ Bench_SealedClass/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Bench>();
+
+public class Bench
+{
+    private BaseClass _baseClass = new BaseClass();
+    private Inheritor _inheritor = new Inheritor();
+    private InheritorSealed _inheritorSealed = new InheritorSealed();
+
+    [Benchmark]
+    public void Base_VirtualInt() => _baseClass.VirtualInt();
+
+    [Benchmark]
+    public void Base_Int() => _baseClass.Int();
+
+    [Benchmark]
+    public void Inheritor_VirtualInt() => _inheritor.VirtualInt();
+
+    [Benchmark]
+    public void Inheritor_Int() => _inheritor.Int();
+
+    [Benchmark]
+    public void InheritorSealed_VirtualInt() => _inheritorSealed.VirtualInt();
+
+    [Benchmark]
+    public void InheritorSealed_Int() => _inheritorSealed.Int();
+
+}
+
+public class BaseClass
+{
+    public virtual int VirtualInt() => 5;
+
+    public int Int() => 5;
+}
+
+public sealed class InheritorSealed : BaseClass
+{
+    public override int VirtualInt() => 10;
+
+    public int Int() => 10;
+}
+
+public class Inheritor : BaseClass
+{
+    public override int VirtualInt() => 10;
+
+    public int Int() => 10;
+}
